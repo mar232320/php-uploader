9680c3c27f7f5991e5e74c1b6d8966c031937927
mar232320 <60986110+mar232320@users.noreply.github.com>
2020-04-16 11:06:59 +0200

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -4,6 +4,7 @@ if (is_uploaded_file($_FILES['plik']['tmp_name'])) {
     echo '<br/>';
     move_uploaded_file($_FILES['plik']['tmp_name'],
     $_SERVER['DOCUMENT_ROOT'].'/zdjecia_upload/'.$_FILES['plik']['name']);
+    // /zdjecia_upload/ to nazwa katalogu z przesłanymi plikami. Jezeli chcesz uzyc mojego skryptu na swojej stronie zmien ja na wlasna!
     }
 else {
 echo 'Błąd! Plik nie został przesłany! Sproboj ponownie!';
